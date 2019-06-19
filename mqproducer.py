be381350adda560fc9bc6f3e4736cff896f22ac2
huangzw1 <39298901+huangzw1@users.noreply.github.com>
2019-06-19 13:52:16 +0800

    Update mqproducer.py

--- mqproducer.py
+++ mqproducer.py
@@ -10,17 +10,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='messageq', durable=True)
 
-# message1 = {
-#     "engine": "email",
-#     "content": "just for test",
-#     "subject": "测试邮件",
-#     "mailhost": "smtp.qq.com",
-#     "mailport": 465,
-#     "sender": "592750654@qq.com",
-#     "password": "sbndswrkrjvrbfib",
-#     "receiver": "zewei.huang@cloudtogo.cn"
-# }
-
 
 for i in range(10000):
     message1 = {
@@ -28,7 +17,7 @@ for i in range(10000):
         "notice": "### test11111",
         "mstype": "markdown",
         "title": "test",
-        "ddtoken": "bbd9fe6590f980153c9f929e66c2037fe42cc8b2b46c4323c12273451c96b015",
+        "ddtoken": "xxxxxxx",
         "id": "%d" % (i)
     }
     message = json.dumps(message1)
